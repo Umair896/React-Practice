a0e52e16c77327d5353250791bad1ef808c38869
Umair896 <umairansari896@gmail.com>
2020-08-13 01:30:19 +0530

    Nesting components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Code from "./code";
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Lets Start</h3>
+      <h4>These are the details of students</h4>
+      <Code />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/code.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Code extends Component {
+  render() {
+    return (
+      <div className="details">
+        <div>Name : Md Umair Ansari</div>
+        <div>Age : 23</div>
+        <div>location: Bangalore</div>
+      </div>
+    );
+  }
+}
+
+export default Code;
