e3ef73548edd17c898f3bbd649def34923fabd0e
Umair896 <umairansari896@gmail.com>
2020-08-14 01:38:32 +0530

    stateless components with props

--- src/code.js
+++ src/code.js
@@ -1,19 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Code extends Component {
-  render() {
-    const { info } = this.props;
-    const codeList = info.map((code) => {
-      return (
-        <div className="code" key={code.number}>
-          <div>name : {code.name}</div>
-          <div>age : {code.age}</div>
-          <div>location: {code.location}</div>
-        </div>
-      );
-    });
-    return <div className="code-list">{codeList}</div>;
-  }
-}
+const Code = ({ info }) => {
+  // const { info } = props;
+  const codeList = info.map((code) => {
+    return (
+      <div className="code" key={code.number}>
+        <div>name : {code.name}</div>
+        <div>age : {code.age}</div>
+        <div>location: {code.location}</div>
+      </div>
+    );
+  });
+  return <div className="code-list">{codeList}</div>;
+};
 
 export default Code;
