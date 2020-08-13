0645475b0f54647a8dcee521a072f437b9de355e
Umair896 <umairansari896@gmail.com>
2020-08-14 01:56:57 +0530

    Conditional output

--- src/code.js
+++ src/code.js
@@ -1,15 +1,27 @@
 import React from "react";
 
 const Code = ({ info }) => {
-  // const { info } = props;
+  // const codeList = info.map((code) => {
+  //   if (code.age < 24) {
+  //     return (
+  //       <div className="code" key={code.number}>
+  //         <div>name : {code.name}</div>
+  //         <div>age : {code.age}</div>
+  //         <div>location: {code.location}</div>
+  //       </div>
+  //     );
+  //   } else {
+  //     return null;
+  //   }
+  // });
   const codeList = info.map((code) => {
-    return (
+    return code.age < 24 ? (
       <div className="code" key={code.number}>
         <div>name : {code.name}</div>
         <div>age : {code.age}</div>
         <div>location: {code.location}</div>
       </div>
-    );
+    ) : null;
   });
   return <div className="code-list">{codeList}</div>;
 };
